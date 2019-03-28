a298a3c0c629f59d8ecb63dadbd2e72690bbd3cf
Fay Wang <wcj233@sina.com>
2019-03-28 16:57:24 +0800

    Add project files.

---
+++ NavApp1/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NavApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+//cache
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page2), name.Text);
+        }
+        private static int _clicks = 0;
+        private void Increase_Click(object sender, RoutedEventArgs e)
+        {
+            _clicks += 1;
+            clickTextBlock.Text = "Number of Clicks: " + _clicks;
+
+        }
+
+        private void Decrease_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
